d361be7b59bb66f71c8188fe53998bc202f33fa0
kaushiki-singh <kaushiki12.qms@gmail.com>
2021-04-06 22:27:39 +0530

    inital

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,85 @@ const App = () => {
   const [ballPosition,setBallPosition] = useState({
     left: "0px",
     top: "0px",
+    position:"absolute"
   });
+
+  useEffect(()=>{
+    const keyListener = document.addEventListener("keydown",(event)=>{
+        // console.log(event.keyCode);
+        switch(event.keyCode){
+            case 39:
+                setBallPosition((ballPosition) => {
+                  return {  left: ballPosition.left+5+"px",
+                    top: ballPosition.top+"px",
+                  }
+
+                });
+                break;
+                case 40:
+                    setBallPosition((ballPosition) => {
+                        return {   left: ballPosition.left+"px",
+                        top: ballPosition.top+5+"px",
+                        }
+                    });
+                    break;
+                    case 37:
+                        setBallPosition((ballPosition) => {
+                            return {    left: ballPosition.left-5+"px",
+                            top: ballPosition.top+"px",
+                            }
+                        });
+                        break;
+                        case 38:
+                            setBallPosition((ballPosition) => {
+                              return { left: ballPosition.left+"px",
+                                top: ballPosition.top-5+"px",
+                              }
+                            })
+                            break;
+        }
+    });
+    //cleanup function
+    //it will remove the previous event listener
+    return () => document.removeEventListener("keydown", keyListener);
+
+},[]);
+
   const reset = () => {};
   const renderChoice = () => {};
+  const renderBallOrButton = () =>{
+    if(renderBall){
+      return( 
+      <div className="ball"
+      style={{
+        left: ballPosition.left,
+        top: ballPosition.top,
+        position: "absolute"
+      }}></div>)
+    }
+    else{
+      return <button onClick={start} className="start">
+      Start
+    </button>
+    }
+  }
+  const start =()=>{
+    setRenderBall(true);
+  }
+  const reset =() => {
+    setRenderBall(false);
+    setBallPosition({
+      left: "0px",
+    top: "0px",
+    position:"absolute"
+    })
+  }
 
   return (
     <div className="playground">
+      <div className="playground">
+        {renderBallOrButton()}
+      </div>
       <button onClick={reset} className="reset">
         Reset
       </button>
