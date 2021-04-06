3df0612932bcc271a070a65731c04bec0f6adda4
kaushiki-singh <kaushiki12.qms@gmail.com>
2021-04-06 23:13:47 +0530

    changed position

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -6,8 +6,8 @@ const App = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+    left: 0,
+    top: 0,
     position:"absolute"
   });
 
@@ -17,30 +17,30 @@ const App = () => {
         switch(event.keyCode){
             case 39:
                 setBallPosition((ballPosition) => {
-                  return {  left: ballPosition.left+5+"px",
-                    top: ballPosition.top+"px",
+                  return {  left: ballPosition.left+5,
+                    top: ballPosition.top,
                   }
 
                 });
                 break;
                 case 40:
                     setBallPosition((ballPosition) => {
-                        return {   left: ballPosition.left+"px",
-                        top: ballPosition.top+5+"px",
+                        return {   left: ballPosition.left,
+                        top: ballPosition.top+5,
                         }
                     });
                     break;
                     case 37:
                         setBallPosition((ballPosition) => {
-                            return {    left: ballPosition.left-5+"px",
-                            top: ballPosition.top+"px",
+                            return {    left: ballPosition.left-5,
+                            top: ballPosition.top,
                             }
                         });
                         break;
                         case 38:
                             setBallPosition((ballPosition) => {
-                              return { left: ballPosition.left+"px",
-                                top: ballPosition.top-5+"px",
+                              return { left: ballPosition.left,
+                                top: ballPosition.top-5,
                               }
                             })
                             break;
@@ -52,15 +52,22 @@ const App = () => {
 
 },[]);
 
-  const reset = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setBallPosition({
+      left: "0px",
+    top: "0px",
+    position:"absolute"
+    })
+  };
   const renderChoice = () => {};
   const renderBallOrButton = () =>{
     if(renderBall){
       return( 
       <div className="ball"
       style={{
-        left: ballPosition.left,
-        top: ballPosition.top,
+        left: ballPosition.left+"px",
+        top: ballPosition.top+"px",
         position: "absolute"
       }}></div>)
     }
@@ -73,14 +80,7 @@ const App = () => {
   const start =()=>{
     setRenderBall(true);
   }
-  const reset =() => {
-    setRenderBall(false);
-    setBallPosition({
-      left: "0px",
-    top: "0px",
-    position:"absolute"
-    })
-  }
+  
 
   return (
     <div className="playground">
